079d9eb59b8c629739a9481f2c24c80163cf43e9
Nedelis <dimoshunya@gmail.com>
2025-11-14 13:40:41 +0300

    deleted empty line

--- hih_project/hih_project/urls.py
+++ hih_project/hih_project/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, URLPattern
 from main.views import index, about, create
 
 urlpatterns: list[URLPattern]  = [
-    
     path('', index, name = 'home'),
     path('about/', about, name = 'about'),
     path('create/', create, name = 'create'),
